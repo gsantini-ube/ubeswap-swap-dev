364c33ef8a7b29729e2ecb17db470ddf6c959020
gsantini-ube <gsantini@ubeswap.org>
2022-06-28 13:49:21 -0700

    test swap module in a new react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Swap from 'ubeswap-swap-dev';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Swap/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
